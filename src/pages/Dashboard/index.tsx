f6e0f70860e8f283d8fe3e235cb61cfffe102148
Felipe Fonseca Correa <felipefcfcfc@gmail.com>
2023-12-08 21:22:37 -0300

    socorro

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -8,8 +8,6 @@ import { Id } from "../../../node_modules/react-toastify/dist/index";
 export default function Dashboard(){
 
   return(
-    <iframe id="dashboard" title="PowerBiSasc2" width="1140" height="541.25" 
-    src="https://app.powerbi.com/reportEmbed?reportId=6010dfaf-b747-447d-84c4-e19dd7f6eac5&autoAuth=true&ctid=79553679-86d7-4827-88ba-d5dd74a01773" 
-    frameborder="0" allowFullScreen="true"></iframe>
+    <iframe id="dashboard" title="PowerBiSasc2" width="1140" height="541.25" src="https://app.powerbi.com/reportEmbed?reportId=6010dfaf-b747-447d-84c4-e19dd7f6eac5&autoAuth=true&ctid=79553679-86d7-4827-88ba-d5dd74a01773" frameborder="0" allowFullScreen="true"></iframe>
   ); 
 }
