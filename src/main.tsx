b53a2e14bed81459ce1ec927010b30ba19d04401
t-rindade <tr.ndade3@gmail.com>
2024-02-09 21:47:54 -0300

    issoai

--- src/main.tsx
+++ src/main.tsx
@@ -16,7 +16,7 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
-      <Route path='Login' element = { <Login/> }/>
+        <Route path="/" element={<Login />} />
         <Route
           path="home"
           element={
