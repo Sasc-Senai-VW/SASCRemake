af1da3150857217a50137a494bc14d6f3ee2833b
t-rindade <tr.ndade3@gmail.com>
2024-02-21 20:05:23 -0300

    final

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -13,26 +13,36 @@ export default function Home() {
     <>
       <div id="home">
         <div className="linhasHome" id="linha1">
-        <div className="videoYoutube">
-          <YoutubeEmbed embedId="p3j_4-WA8FQ"></YoutubeEmbed>
+          <div className="videoYoutube">
+            <YoutubeEmbed embedId="p3j_4-WA8FQ"></YoutubeEmbed>
           </div>
 
           <div className="videoYoutube">
-          <YoutubeEmbed2 embedId="p3j_4-WA8FQ"></YoutubeEmbed2>
+            <YoutubeEmbed2 embedId="p3j_4-WA8FQ"></YoutubeEmbed2>
           </div>
-
         </div>
 
         <div className="linhasHome" id="linha2">
-        <iframe id="dashboradHome" title="PowerBiSascHome" width="1140" height="541.25" src="https://app.powerbi.com/reportEmbed?reportId=5dcc2254-405e-4ecb-9910-bb6a3cbba750&autoAuth=true&ctid=79553679-86d7-4827-88ba-d5dd74a01773" frameborder="0" allowFullScreen="true"></iframe>
+          <iframe
+            id="dashboradHome"
+            title="PowerBiSascHome"
+            width="1140"
+            height="541.25"
+            src="https://app.powerbi.com/reportEmbed?reportId=5dcc2254-405e-4ecb-9910-bb6a3cbba750&autoAuth=true&ctid=79553679-86d7-4827-88ba-d5dd74a01773"
+            frameborder="0"
+            allowFullScreen="true"
+          ></iframe>
         </div>
-        
-        <a  href="https://borainvestir.b3.com.br/noticias/esg/entenda-o-que-sao-e-como-funcionam-os-creditos-de-carbono/?gclid=CjwKCAiAtt2tBhBDEiwALZuhAHNnTDsRPruf4xOdpa95JPU9WdRXyzqgn-NW3MmdkjOM8SuoAsiHeBoCxwAQAvD_BwE" target="_blank">
-            <div className="linhasHome" id="frameCredito"> </div>
+
+        <a
+          href="https://borainvestir.b3.com.br/noticias/esg/entenda-o-que-sao-e-como-funcionam-os-creditos-de-carbono/?gclid=CjwKCAiAtt2tBhBDEiwALZuhAHNnTDsRPruf4xOdpa95JPU9WdRXyzqgn-NW3MmdkjOM8SuoAsiHeBoCxwAQAvD_BwE"
+          target="_blank"
+        >
+          <div className="linhasHome" id="frameCredito">
+            {" "}
+          </div>
         </a>
-
       </div>
-
     </>
   );
 }
